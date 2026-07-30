211921c7e7fe6a7317fc93e1f92a658c9c418e7f
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2026-07-30 16:46:57 -0300

    Alterações no CSS, TS e HTML

---
+++ src/app/lista-produtos/item.ts
@@ -0,0 +1,13 @@
+export class Item{
+    idProduto?: number
+    descricaoProduto?: string
+    valorUnitario? : number
+    statusSelecionado: boolean = false
+
+    /*constructor(idProduto: number, descricaoProduto: string, valorUnitario: number) {
+        this.idProduto = idProduto
+        this.descricaoProduto = descricaoProduto
+        this.valorUnitario = valorUnitario
+    }*/
+
+}
